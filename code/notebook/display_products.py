5e4021d5bbd363e263c1db440bf705d9c82fd519
mvellera <melvin.vellera@gmail.com>
2022-01-10 12:58:32 -0800

    Update

--- code/notebook/display_products.py
+++ code/notebook/display_products.py
@@ -10,15 +10,14 @@ class DisplayProducts():
         for ingredient in recipe_ingredients:
             self.display_products_ingredient(ingredient)
     
-    def display_products_ingredient(self, ingredient, n=3):
+    def display_products_ingredient(self, ingredient):
         preprocessed_ingredient = preprocess([ingredient])
-        tcin_list = self.ranker.rank_products_ingredient(preprocessed_ingredient, max_rank=n)
+        tcin_list = self.ranker.rank_products_ingredient(preprocessed_ingredient)
         n_tcin = len(tcin_list)
         images = self.mapper.get_image_list(tcin_list)
         titles = self.mapper.get_title_list(tcin_list)
         
-        print(ingredient) 
-        print(tcin_list)
+        print(ingredient)
         plt.figure(figsize=(20,10))
         columns = 3
         for i in range(n_tcin):
@@ -28,50 +27,3 @@ class DisplayProducts():
             if not isinstance(images[i], float):
                 plt.imshow(images[i])
         plt.show()
-
-    def display_products_recipe_tcins(self, tcin_lists):
-        for tcin_list in tcin_lists:
-            self.display_products_ingredient_tcins(tcin_list)
-
-    def display_products_ingredient_tcins(self, tcin_list):
-        n_tcin = len(tcin_list)
-        images = self.mapper.get_image_list(tcin_list)
-        titles = self.mapper.get_title_list(tcin_list)
-        
-        print(tcin_list)
-        plt.figure(figsize=(20,10))
-        columns = 3
-        for i in range(n_tcin):
-            plt.subplot(int(n_tcin / columns) + 1, columns, i + 1)
-            plt.axis('off')
-            plt.title(titles[i])
-            if not isinstance(images[i], float):
-                plt.imshow(images[i])
-        plt.show()
-
-    def display_products_df(self, display_df, n=3):
-        unique_ingredients = display_df['ingredient'].unique()
-        for ingredient in unique_ingredients:
-            tcin_list = display_df[display_df['ingredient'] == ingredient]['tcin'].values[0:n]
-            recommended_qty_list = display_df[display_df['ingredient'] == ingredient]['recommended_qty'].values[0:n]
-            price_list = display_df[display_df['ingredient'] == ingredient]['price'].values[0:n]
-
-            n_tcin = len(tcin_list)
-            images = self.mapper.get_image_list(tcin_list)
-            titles = self.mapper.get_title_list(tcin_list)
-            
-            print(ingredient) 
-            print(tcin_list)
-            plt.figure(figsize=(20,10))
-            columns = 3
-            for i in range(n_tcin):
-                ax = plt.subplot(int(n_tcin / columns) + 1, columns, i + 1)
-                plt.axis('off')
-                plt.title(titles[i])
-                if not isinstance(images[i], float):
-                    plt.imshow(images[i])
-                ax.text(0.1, -0.1, f"Recommended Qty: {recommended_qty_list[i]: .0f}", size=12, ha="left", 
-                        transform=ax.transAxes)
-                ax.text(0.1, -0.17, f"Price: {price_list[i]: .2f}", size=12, ha="left", 
-                        transform=ax.transAxes)
-            plt.show()
